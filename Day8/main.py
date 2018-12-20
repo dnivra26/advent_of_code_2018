9cc3873f417dd241d5a0dab314421d521e88d80e
Arvind Thangamani <dnivra26@gmail.com>
2018-12-20 15:04:53 +0530

    Day 8 - part 1 solution

--- Day8/main.py
+++ Day8/main.py
@@ -1,17 +1,20 @@
-file = open("input2.txt", "r")
-
+file = open("input.txt", "r")
 values = file.read().splitlines()[0].split(" ")
 
-def iterate(index):
+def iter(index, metadata):
+    if index >= len(values):
+        return index, metadata
+
     child_count = int(values[index])
     metadata_count = int(values[index + 1])
     
-    print("node index: ", index, "child count: ", child_count, "metadata count: ", metadata_count)
     if child_count == 0:
-        return index + 2 + metadata_count
+        return index + 2 + metadata_count, metadata + values[index+2 : index+2+metadata_count]
     else:
         depth = index + 2
-        for child in range(0, child_count):
-            depth = iterate(depth)
+        for _ in range(0, child_count):
+            depth, metadata = iter(depth, metadata)
+        return depth+ metadata_count, metadata + values[depth : depth+metadata_count]
 
-iterate(0)
+depth, metadata = iter(0, [])
+print(sum(list(map(int, metadata))))
